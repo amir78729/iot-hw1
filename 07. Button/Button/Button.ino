0823fc8defa5599f36c0098bbc5a887421b88dd8
Amirhossein Alibakhshi <amirhosseinalibakhshi@gmail.com>
2022-03-10 22:17:24 +0330

    adding comments

--- 07. Button/Button/Button.ino
+++ 07. Button/Button/Button.ino
@@ -1,7 +1,7 @@
-const int LDR_PIN = A0;
-const int LED_PIN = 5;
+const int LDR_PIN = A0; // assiging LDR pin: A0
+const int LED_PIN = 5;  // assiging LED pin: D1
 
-bool isActive = false;
+bool isActive = false; // a boolean variable for saving the LED's state
 
 void setup() {
   Serial.begin(9600);
@@ -9,17 +9,17 @@ void setup() {
 }
 
 void loop() {
-  int sensor_value = analogRead(LDR_PIN);
+  int sensor_value = analogRead(LDR_PIN); // reading the LDR value
   Serial.print("input value: ");
   Serial.print(sensor_value);
   Serial.print("  \tLED is ");
   Serial.print(isActive ? "ON" : "OFF");
   Serial.print("\t");
-  if (sensor_value < 100) {
+  if (sensor_value < 100) { // putting the LDR in a dark place...
     Serial.print("*STATUS CHANGED!!*");
     isActive = !isActive;
-    digitalWrite(LED_PIN, isActive ? HIGH : LOW);
-    delay(1000);
+    digitalWrite(LED_PIN, isActive ? HIGH : LOW); // changing the state of LED
+    delay(1000); // waiting for the LDR to be in a lighter place
   }
   Serial.println("");
   delay(200);
