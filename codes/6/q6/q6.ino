3a9189d522cc2693ae89ef4ea434a6647244f18d
Amirhossein Alibakhshi <amirhosseinalibakhshi@gmail.com>
2022-03-10 16:41:15 +0330

    adding q6

---
+++ codes/6/q6/q6.ino
@@ -0,0 +1,11 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("+------------------------+");
+  Serial.println("| Amirhossein Alibakhshi |");
+  Serial.println("|      ID : 9731096      |");
+  Serial.println("+------------------------+");
+  delay(1000);
+}
