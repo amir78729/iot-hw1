cd4ae75755cc2f7fbc9cd5a242ae294f44fe4e59
Negin Hajisobhani <51993060+neginhsobhani@users.noreply.github.com>
2022-03-18 00:44:11 +0330

    adding screen light

--- 10. screen Light/screen_Light.ino
+++ 10. screen Light/screen_Light.ino
@@ -1,9 +1,15 @@
+const int LDR_PIN = A0; // assiging LDR pin: A0
+const int LED_PIN = 5;  // assiging LED pin: D1
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  int brightness = analogRead(LDR_PIN); //value between 0 and 1023
+  analogWrite(LED_PIN, (1023 - brightness) / 4);
+  Serial.println(brightness);
 }
