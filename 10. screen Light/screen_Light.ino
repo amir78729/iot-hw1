be9f9ba3f6de6874f9a8f017853b6419d83c84be
Negin Hajisobhani <51993060+neginhsobhani@users.noreply.github.com>
2022-03-18 00:45:37 +0330

    adding comments

--- 10. screen Light/screen_Light.ino
+++ 10. screen Light/screen_Light.ino
@@ -9,7 +9,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int brightness = analogRead(LDR_PIN); //value between 0 and 1023
-  analogWrite(LED_PIN, (1023 - brightness) / 4);
+  int brightness = analogRead(LDR_PIN); // value between 0 and 1023
+  analogWrite(LED_PIN, (1023 - brightness) / 4); // PWM value is between 0 and 255 so we divide brightness value by 4
   Serial.println(brightness);
 }
