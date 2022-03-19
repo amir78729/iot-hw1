2c8efab23208a52932fdb2463ea5a7f54a75192d
Amirhossein Alibakhshi <amirhosseinalibakhshi@gmail.com>
2022-03-19 23:53:26 +0330

    refactoring line detaction and adding part 2

---
+++ 08. Line Detection/Part 2/Line_Detection/Line_Detection.ino
@@ -0,0 +1,21 @@
+const int LDR_PIN = A0; // assiging LDR pin: A0
+const int LED_PIN = 5; // assiging LED pin: D1
+const int BUZZER_PIN = 4; // assiging buzzer pin: D2
+const int BRIGHTNESS_THRESHHOLD = 40; // assiging buzzer pin: D2
+
+bool isActive = false;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+}
+
+void loop() {
+  int sensor_value = map(analogRead(LDR_PIN), 4, 1024, 0, 100); // mapping analog value between [0, 100]
+  Serial.print("input value: ");
+  Serial.println(sensor_value);
+  digitalWrite(BUZZER_PIN, sensor_value > BRIGHTNESS_THRESHHOLD ? HIGH : LOW); // activate buzzer if the line was not detected
+  digitalWrite(LED_PIN, sensor_value <= BRIGHTNESS_THRESHHOLD ? HIGH : LOW); // activate LED if the line was detected
+  delay(200);
+}
