58da8319d658c12a8d3a6dc92f7210b05038de38
Amirhossein Alibakhshi <amirhosseinalibakhshi@gmail.com>
2022-03-19 23:48:59 +0330

    adding PDF file and renaming the BRIGHTNESS_THRESHOLD

--- 08. Line Detection/Line_Detection/Line_Detection.ino
+++ 08. Line Detection/Line_Detection/Line_Detection.ino
@@ -1,7 +1,7 @@
 const int LDR_PIN = A0; // assiging LDR pin: A0
 const int LED_PIN = 5; // assiging LED pin: D1
 const int BUZZER_PIN = 4; // assiging buzzer pin: D2
-
+const int BRIGHTNESS_TRASHHOLD = 40; // assiging buzzer pin: D2
 
 bool isActive = false;
 
@@ -15,7 +15,7 @@ void loop() {
   int sensor_value = map(analogRead(LDR_PIN), 4, 1024, 0, 100); // mapping analog value between [0, 100]
   Serial.print("input value: ");
   Serial.println(sensor_value);
-  digitalWrite(BUZZER_PIN, sensor_value > 40 ? HIGH : LOW); // activate buzzer if the line was not detected
-  digitalWrite(LED_PIN, sensor_value <= 40 ? HIGH : LOW); // activate LED if the line was detected
+  digitalWrite(BUZZER_PIN, sensor_value > BRIGHTNESS_TRASHHOLD ? HIGH : LOW); // activate buzzer if the line was not detected
+  digitalWrite(LED_PIN, sensor_value <= BRIGHTNESS_TRASHHOLD ? HIGH : LOW); // activate LED if the line was detected
   delay(200);
 }
